ecba3984d667193f142308b1bcb7c6c66ade6afa
KIM-JS-95 <baugh248730@gmail.com>
2021-04-16 20:33:13 +0900

    2021-04-16

--- untitled/src/test/java/KAKAO/EntryKey.java
+++ untitled/src/test/java/KAKAO/EntryKey.java
@@ -24,18 +24,20 @@ public class EntryKey {
         public int compare(Integer o1, Integer o2) {
             int x = countBits(o1);
             int y = countBits(o2);
+
             if (x > y) {
                 return 1;
             } else if (x < y) {
                 return -1;
-            }
-            return 0;
+            } else
+                return 0;
         }
     };
 
     static boolean check(String[][] relation, int rowsize, int colsize, int subset) {
+
         for (int a = 0; a < rowsize - 1; a++) {
-            for (int b = a+1; b < rowsize; b++) {
+            for (int b = a + 1; b < rowsize; b++) {
                 boolean isSame = true;
                 for (int k = 0; k < colsize; k++) {
                     if ((subset & 1 << k) == 0) continue;
@@ -62,22 +64,20 @@ public class EntryKey {
             if (check(relation, rowsize, colsize, i) == true) {
                 candidates.add(i);
             }
+        }
+        Collections.sort(candidates, comp);
 
-            Collections.sort(candidates, comp);
+        while (candidates.size() != 0) {
+            int n = candidates.remove(0);
+            answer++;
 
-            while (candidates.size() != 0) {
-                int n = candidates.remove(0);
-                answer++;
-
-                for (Iterator<Integer> it = candidates.iterator(); it.hasNext(); ) {
-                    int c = it.next();
-
-                    if ((n & c) == n)
-                        it.remove();
-
-                }
+            for (Iterator<Integer> it = candidates.iterator(); it.hasNext(); System.out.println(it.hasNext())) {
+                int c = it.next();
+                if ((n & c) == n)
+                    it.remove();
             }
         }
+
         return answer;
     }
 }
