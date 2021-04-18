d3665da434cdee8a8079d5a9fe376a717b7ee0d3
KIM-JS-95 <baugh248730@gmail.com>
2021-04-18 19:54:14 +0900

    2021-04-18

--- untitled/src/test/java/KAKAO/Failure_rate.java
+++ untitled/src/test/java/KAKAO/Failure_rate.java
@@ -18,7 +18,6 @@ public class Failure_rate {
         int[] answer = new int[N];
         HashMap<Integer, Double> failmap = new HashMap<Integer, Double>();
 
-
         for (int i = 1; i <= N; i++) {
             int stage = i;
             int incompletePlayers = 0;
@@ -26,24 +25,19 @@ public class Failure_rate {
 
             for (int j = 0; j < stages.length; j++) {
                 int player = stages[j];
-
                 if (stage == player) {
                     incompletePlayers++;
                 }
                 if (player >= stage) {
                     curretStageTotalPlayers++;
                 }
-
             }
 
             double failureRate = 0;
-
             if (incompletePlayers != 0 && curretStageTotalPlayers != 0) {
                 failureRate = (incompletePlayers / (double) curretStageTotalPlayers);
             }
             failmap.put(stage, failureRate);
-
-
         }
 
         for (int i = 0; i < N; i++) {
@@ -60,4 +54,5 @@ public class Failure_rate {
         }
         return answer;
     }
+
 }
---
+++ untitled/src/test/java/KAKAO/ThatSong.java
@@ -0,0 +1,22 @@
+package KAKAO;
+
+public class ThatSong {
+
+public static void main(String[] args){
+
+    String m = "ABCDEFG";
+    String[] musicinfos =  {"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"};
+
+    solution(m,musicinfos);
+
+}
+
+    public static String solution(String m, String[] musicinfos) {
+
+
+    String[]  musicinfos
+
+        String answer = "";
+        return answer;
+    }
+}
