257106eb27348d6c5894f9a13d652ae3c63b6c3b
KIM-JS-95 <baugh248730@gmail.com>
2021-04-22 21:59:55 +0900

    2021-04-22

--- untitled/src/test/java/BACKJOON/back_2178.java
+++ untitled/src/test/java/BACKJOON/back_2178.java
@@ -11,51 +11,72 @@ public class back_2178 {
 
     public static int N;
     public static int M;
-
     public static int[][] grid;
     public static boolean[][] flag;
 
+
     public static void main(String[] args) throws IOException {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
         M = sc.nextInt();
 
-
+        sc.nextLine();
         grid = new int[N][M];
         flag = new boolean[N][M];
 
-        for (int i=0; i<N; i++) {
-            String temp = sc.next();
-            for (int j=0; j<M; j++) {
-                grid[i][j] = temp.charAt(j) - 48; // ASCII Code 48~57 (숫자) 문자를 숫자로 변환
-                 }
+        for (int i = 0; i < N; i++) {
+            String temp = sc.nextLine();
+            for (int j = 0; j < M; j++) {
+                grid[i][j] = temp.charAt(j) - '0';
+                flag[i][j] = false;
+            }
         }
 
+        flag[0][0] = true;
+        bfs(0, 0);
 
-
-        bfs(0,0);
-
+        System.out.println(grid[N - 1][M - 1]);
     }
 
-    public static void bfs(int x, int y){
-        Queue<Integer> row = new LinkedList<>();
-        Queue<Integer> col = new LinkedList<>();
+    public static void bfs(int x, int y) {
 
-        row.add(x);
-        col.add(y);
+        int[] xd = {-1, 0, 1, 0};
+        int[] yd = {0, -1, 0, 1};
+        Queue<Dot> q = new LinkedList<Dot>();
+        q.add(new Dot(x, y));
 
-        while(!row.isEmpty() && !col.isEmpty()){
-            x = row.poll();
-            y = col.poll();
-            flag[x][y] = true;
+        while (!q.isEmpty()) {
+            Dot d = q.poll();
 
-            for(int i=0; i< 4 ; i++){
+            for (int i = 0; i < 4; i++) {
+                int nextX = d.x + xd[i];
+                int nextY = d.y + yd[i];
 
+                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+                    continue;
+                }
+                if (flag[nextX][nextY] || grid[nextX][nextY] == 0) {
+                    continue;
+                }
+
+                q.add(new Dot(nextX,nextY));
+
+                grid[nextX][nextY] = grid[d.x][d.y]+1;
+
+                flag[nextX][nextY] = true;
             }
         }
     }
 
-
 }
 
+class Dot {
+    int x;
+    int y;
+
+    Dot(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
