8d84e322c2d0256e2e08547de8aedb906edfd59f
KIM-JS-95 <baugh248730@gmail.com>
2021-04-23 23:50:25 +0900

    2021-04-23

---
+++ untitled/src/test/java/BACKJOON/back_10451.java
@@ -0,0 +1,11 @@
+package BACKJOON;
+
+import java.io.IOException;
+
+public class back_10451 {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+}
--- untitled/src/test/java/BACKJOON/back_2178.java
+++ untitled/src/test/java/BACKJOON/back_2178.java
@@ -1,10 +1,10 @@
 package BACKJOON;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
 
 public class back_2178 {
