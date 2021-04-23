f25f70296e11dd91e499922eb6913d7760019543
KIM-JS-95 <baugh248730@gmail.com>
2021-04-24 00:19:01 +0900

    2021-04-24

--- untitled/src/test/java/BACKJOON/back_10451.java
+++ untitled/src/test/java/BACKJOON/back_10451.java
@@ -1,11 +1,45 @@
 package BACKJOON;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class back_10451 {
 
-    public static void main(String[] args) throws IOException {
+    public static int[] arr;
+    public static boolean[] flag;
 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int  t =sc.nextInt();
+        int cnt =0;
+
+        for(int i=0; i<t; i++){
+            int N = sc.nextInt();
+            arr = new int[N+1];
+            flag = new boolean[N+1];
+
+            for(int j=1; j<N+1; j++){
+                arr[j]= sc.nextInt();
+            }
+            for(int j=1; j<N+1; j++){
+                if(!flag[j])
+                cnt +=solution(j);
+            }
+
+        }
+        System.out.println(cnt);
     }
 
+    public static int solution(int i){
+
+        if(i==arr[i] || flag[i])
+            return 1;
+
+        flag[i] = true;
+        return solution(arr[i]);
+
+    }
 }
