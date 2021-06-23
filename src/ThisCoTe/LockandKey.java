3f8c11ec1bf062c1ff9ac187932a45c77e216964
KIM-JS-95 <baugh248730@gmail.com>
2021-06-24 01:04:24 +0900

    2021-06

--- src/ThisCoTe/LockandKey.java
+++ src/ThisCoTe/LockandKey.java
@@ -1,24 +1,20 @@
-package KAKAO;
+package ThisCoTe;
 
-public class Lock_and_Key {
-
+public class LockandKey {
     public static void main(String[] args) {
         int[][] key = {{0, 0, 0}, {1, 0, 0}, {0, 1, 1}};
         int[][] lock = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
-
-
         solution(key, lock);
     }
 
-    public static boolean solution(int[][] key, int[][] lock) {
 
+    public static boolean solution(int[][] key, int[][] lock) {
         int offset = key.length - 1;
 
         for (int r = 0; r < offset + lock.length; r++) {
             for (int c = 0; c < offset + lock.length; c++) {
 
-                // rotation
-                for (int rot = 0; rot < 4; ++rot) {
+                for (int rot = 0; rot < 4; rot++) {
                     int[][] arr = new int[58][58];
 
                     for (int i = 0; i < lock.length; i++) {
@@ -29,14 +25,13 @@ public class Lock_and_Key {
 
                     match(arr, key, rot, r, c);
                     if (check(arr, offset, lock.length)) {
+                        System.out.println("true");
                         return true;
                     }
                 }
             }
-
         }
-
-
+        System.out.println("false");
         return false;
     }
 
@@ -53,22 +48,20 @@ public class Lock_and_Key {
     }
 
     public static void match(int[][] arr, int[][] key, int rot, int r, int c) {
-        int n = key.length;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        int n = key.length - 1; //2
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= n; j++) {
                 if (rot == 0) {
                     arr[r + i][c + j] += key[i][j];
                 } else if (rot == 1) { //90
-                    arr[r + i][c + j] += key[j][n - 1 - i];
+                    arr[r + i][c + j] += key[j][n - i];
                 } else if (rot == 2) { //180
-                    arr[r + i][c + j] += key[n - 1 - i][n - 1 - j];
-                } else {
-                    arr[r + i][c + j] += key[n - 1 - j][i];
+                    arr[r + i][c + j] += key[n - i][n - j];
+                } else { // 270
+                    arr[r + i][c + j] += key[n - j][i];
                 }
             }
         }
     }
-
 }
-
-
